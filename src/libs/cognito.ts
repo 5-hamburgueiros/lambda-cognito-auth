24caba66c3fd17b61e9f16d19090af5120e92a74
Miranda <vinniimiranda@gmail.com>
2024-03-10 13:45:19 -0300

    fix: remove credentials

--- src/libs/cognito.ts
+++ src/libs/cognito.ts
@@ -3,8 +3,4 @@ import 'dotenv/config';
 
 export const cognitoClient = new CognitoIdentityProviderClient({
   region: 'us-east-1',
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  },
 });
