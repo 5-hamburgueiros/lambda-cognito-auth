03ca42e79eb81f49c883ff9e3f21b2b0d7ee36cf
Miranda <vinniimiranda@gmail.com>
2024-03-18 10:20:14 -0300

    fix: return to use simple challenge

--- src/functions/createChallenge/handler.ts
+++ src/functions/createChallenge/handler.ts
@@ -1,6 +1,18 @@
 export const handle = async (event: any) => {
-  event.response.issueTokens = true;
-  event.response.failAuthentication = false;
+  console.log(event);
+  event.response.publicChallengeParameters = {
+    email: event.request.userAttributes.email,
+    maxAttempts: 3,
+    attempts: event.request.session,
+    attemptsLeft: 3 - event.request.session.length,
+  };
+
+  event.response.privateChallengeParameters = {
+    cpf: event.request.userAttributes.cpf,
+  };
+
+  event.response.challengeMetadata = `${event.request.userAttributes.cpf}`;
+
   return event;
 };
 
--- src/functions/defineChallenge/handler.ts
+++ src/functions/defineChallenge/handler.ts
@@ -1,31 +1,7 @@
-const MAX_ATTEMPTS = 3;
-
 export const handle = async (event) => {
   console.log(event);
   event.response.issueTokens = true;
   event.response.failAuthentication = false;
-  const attempts = event.request.session.length;
-  const lastAttempt = event.request.session.slice(-1)[0];
-
-  if (event.request.session && event.request.session.find((attempt) => attempt.challengeName !== 'CUSTOM_CHALLENGE')) {
-    event.response.issueTokens = false;
-    event.response.failAuthentication = true;
-  } else if (attempts >= MAX_ATTEMPTS && lastAttempt.challengeResult === false) {
-    event.response.issueTokens = false;
-    event.response.failAuthentication = true;
-  } else if (
-    attempts >= 1 &&
-    lastAttempt.challengeName === 'CUSTOM_CHALLENGE' &&
-    lastAttempt.challengeResult === true
-  ) {
-    event.response.issueTokens = true;
-    event.response.failAuthentication = false;
-  } else {
-    event.response.issueTokens = false;
-    event.response.failAuthentication = false;
-    event.response.challengeName = 'CUSTOM_CHALLENGE';
-  }
-
   return event;
 };
 
